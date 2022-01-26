33398c74213209732a326cece8c6d9897c580f58
PoND\Sakon <sakon.kpt@gmail.com>
2022-01-26 21:59:18 +0700

    Add Name: Sakon Kanpitak

--- vue_project/project/src/App.vue
+++ vue_project/project/src/App.vue
@@ -2,17 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <Kittisak />
+  <Sakon />
   
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Kittisak from './components/6106021611142_Kittisak.vue'
+import Sakon from './components/6106021611169_Sakon.vue';
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    Kittisak
+    Kittisak,
+    Sakon
 
   }
 }
---
+++ vue_project/project/src/components/6106021611169_Sakon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="Sakon">
+      <h1>
+          I'm here 6106021611169 สากณ กันพิทักษ์
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Sakon"
+}
+</script>
+
+<style>
+
+</style>
