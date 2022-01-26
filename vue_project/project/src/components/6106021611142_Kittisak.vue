dce4f0c8bab04fa696806a3cfb50f1bc3b5d0147
GUYGAM <guygam31@gmail.com>
2022-01-26 23:09:56 +0700

    edit 555

--- vue_project/project/src/components/6106021611142_Kittisak.vue
+++ vue_project/project/src/components/6106021611142_Kittisak.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="Kittisak">
       <h1>
-          I'm here 6106021611142 กิตติศักดิ์ ธรรมวัฒนา 555 
+          I'm here 6106021611142 กิตติศักดิ์ ธรรมวัฒนา
       </h1>
   </div>
 </template>
