7ae04869278ee6b82b985c45b923d81e96c61d59
Napat Moonprajak <bararas14@gmail.com>
2022-01-28 11:58:57 +0800

    add name

---
+++ vue_project/project/src/components/6106021622039_Napat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Napat">
+    <h1>
+      I'm here 6106021622039 ณภัทร มูลประจักษ์
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Napat",
+};
+</script>
+
+<style></style>
