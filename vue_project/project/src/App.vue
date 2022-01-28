b4690497303051a7f84a46260d56daf41914de37
DESKTOP-6097QKN\Admin <6106021620141@fitm.kmutnb.ac.th>
2022-01-28 11:51:46 +0700

    add Tanapoom & Natnapin.vue

--- vue_project/project/src/App.vue
+++ vue_project/project/src/App.vue
@@ -6,14 +6,18 @@
   <Sakon />
   <Pichayut />
   <Tanatorn />
+  <Tanapoom />
+  <Natnapin />
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 import Kittisak from "./components/6106021611142_Kittisak.vue";
 import Sakon from "./components/6106021611169_Sakon.vue";
 import Pichayut from "./components/6106021612165_Pichayut.vue";
 import Tanatorn from "./components/6106021621041_tanatorn";
+import Tanapoom from "./components/6106021620141_Tanapoom.vue"
+import Natnapin from "./components/6106021610073_Natnapin.vue"
 
 export default {
   name: "App",
@@ -23,6 +27,8 @@ export default {
     Sakon,
     Pichayut,
     Tanatorn,
+    Tanapoom,
+    Natnapin,
   },
 };
 </script>
---
+++ vue_project/project/src/components/6106021620141_Tanapoom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Tanapoom">
+    <h1>
+      I'm here 6106021620141 ธนภูมิ หาดเจียง
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tanapoom",
+};
+</script>
+
+<style></style>
