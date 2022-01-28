a7da943427476834a074b91ae7880559815d8ea9
chitsanupong <chitsanupong.boonserm@gmail.com>
2022-01-28 11:00:08 +0700

    add chitsanupong

---
+++ vue_project/project/src/components/6106021610081_Chitsanupong.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Sakon">
+    <h1>
+      I'm here 6106021610081 ชิษณุพงศ์ บุญเสริม
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sakon",
+};
+</script>
+
+<style></style>
