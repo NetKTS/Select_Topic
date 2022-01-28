a0dca327d7857b8b5a5085c19be8199bb2734a68
Faholic <61242442+Faholic@users.noreply.github.com>
2022-01-28 11:15:53 +0700

    1

---
+++ vue_project/project/src/components/6106021612084_Suchawadee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Suchawadee">
+        <h1>
+            I'm here 6106021612084 สุชาวดี เกิดโภคา
+        </h1>
+    </div>
+</template>
+
+<<script>
+export default {
+    name: "Suchawadee"
+}
+</script>
+
+<style>
+</style>
